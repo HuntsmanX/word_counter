1538247d80d21482ce6087c739508562b81b558d
Artur Yumaev <artur.yumaev@lamoda.ru>
2025-02-16 14:05:45 +0300

    Small code review fixes

--- main.go
+++ main.go
@@ -11,69 +11,87 @@ import (
 	"strings"
 )
 
-const FileName = "hello.txt"
+// не экспортируется из пакета, поэтому можно lowercase
+const defaultFileName = "hello.txt"
 
-type wordFreeq struct {
+type wordFreq struct {
 	Word  string
 	Count int
 }
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	fileName := flag.String("file", FileName, "The name for a file.")
+	fileName := flag.String("file", defaultFileName, "The name for a file.")
 	flag.Parse()
-	logger.Info("Get File", "File", *fileName)
+
+	// используем Fields, все ошибки пишем в lowercase
+	logger.Info("reading file", slog.String("file", *fileName))
+
 	wordMap := map[string]int{}
 	re := regexp.MustCompile(`[\p{L}\d]+`)
 	err := getWordsFromFile(*fileName, re, logger, wordMap)
 	if err != nil {
-		os.Exit(-1)
+		// не было  записи ошибки в лог
+		logger.Error("unable to get words from file", slog.String("err", err.Error()))
+		return
 	}
+
 	wordres := mapToStr(wordMap)
 	sort.SliceStable(wordres, func(i, j int) bool {
 		return wordres[i].Count > wordres[j].Count
 	})
-	logger.Info("Result", "Data", wordres)
-	output(wordres)
+
+	logger.Info("result", "data", wordres)
 }
 
 func fillMap(gm map[string]int, str []string) {
+	// небольшая оптимизация логики
+	// btw, не очень понятно что такое gm
+	// давай именовать более понятными именами
 	for _, w := range str {
-		w = strings.ToLower(w)
-		_, ok := gm[w]
-		if ok {
-			gm[w]++
-		} else {
-			gm[w] = 1
-		}
+		gm[strings.ToLower(w)]++
 	}
 }
 
 func getWordsFromFile(filename string, re *regexp.Regexp, logger *slog.Logger, wm map[string]int) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		logger.Error("Parse File", "Error", err)
-		return err
+		// была некорректная запись в лог
+		logger.Error(
+			"unable to read file",
+			slog.String("filename", filename),
+			slog.String("err", err.Error()),
+		)
+		// оборачиваем ошибку, чтобы знать где именно она возникла по сообщению ошибки
+		return fmt.Errorf("unable to read file: %w", err)
 	}
-	defer file.Close()
+	// потеряли запись ошибки Close в лог
+	defer func() {
+		if err := file.Close(); err != nil {
+			logger.Error("error while closing file", slog.String("err", err.Error()))
+		}
+	}()
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		// в целом норм, ты тут читаешь до \n
+		// но что если строка будет очень большой и файл будет в одну строку и весить 5GB?
+		// в таком случае надо читать буффером
+		// создавать буффер на несколько KB (например 32) и вычитывать кусками
+		// https://stackoverflow.com/a/36111861
 		fillMap(wm, re.FindAllString(scanner.Text(), -1))
 	}
 
 	return nil
 }
 
-func mapToStr(m map[string]int) []wordFreeq {
-	words := make([]wordFreeq, 0)
+// метод называется map to string, но возвращается слайс структур, а не string :)
+func mapToStr(m map[string]int) []wordFreq {
+	// можно добавить capacity, чтобы сразу аллоцировать нужный размер
+	// и не сталкиваться постоянно с реаллокацией и эвакуацией из мапы
+	words := make([]wordFreq, 0, len(m))
 	for k, v := range m {
-		words = append(words, wordFreeq{k, v})
+		words = append(words, wordFreq{k, v})
 	}
 	return words
-}
-
-func output(wordsFre []wordFreeq) {
-	for _, kv := range wordsFre {
-		fmt.Printf("%s %d\n", kv.Word, kv.Count)
-	}
 }
